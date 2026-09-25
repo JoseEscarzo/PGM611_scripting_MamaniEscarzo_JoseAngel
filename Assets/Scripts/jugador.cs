4ef753b9e4d54377249a657007529636f2b97a4a
Jose Escarzo <stichjose0525@gmail.com>
2026-09-25 11:23:22 -0400

    salto del personaje

--- Assets/Scripts/jugador.cs
+++ Assets/Scripts/jugador.cs
@@ -5,6 +5,11 @@ public class jugador : MonoBehaviour
     public float velocidad = 5f;
     private Rigidbody2D rb;
     private float movimiento;
+    public float alturaSalto = 4f;
+    private bool esPiso; // el true quiere decir estamos en el piso y false que estamos en el aire
+    public Transform comprobadorPiso;
+    public float radioComprobadorPiso = 0.1f;
+    public LayerMask layerPiso;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +22,12 @@ public class jugador : MonoBehaviour
         movimiento = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
         if (movimiento != 0) transform.localScale = new Vector3(Mathf.Sign(movimiento), 1, 1);
+        if (Input.GetButtonDown("Jump") && esPiso)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, alturaSalto);
+    }
+    public void FixedUpdate()
+    {
+        esPiso = Physics2D.OverlapCircle(comprobadorPiso.position, radioComprobadorPiso, layerPiso);
     }
 }
 
